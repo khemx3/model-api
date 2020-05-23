bcc297dd99d7596fdd891b26f838abc2c82f8437
Phakapol Chuliang <phakapol.chuliang@gmail.com>
2020-05-23 17:18:15 +0700

    add firestore key and docker compose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from firebase_admin import credentials, firestore, initialize_app
 from model import model
 import os
 
@@ -8,6 +9,11 @@ ml_model.restore_model("models/model_10_weights")
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 8000))
+
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+stock_ref = db.collection('stock')
 
 @app.route('/')
 def hello_world():
@@ -25,6 +31,34 @@ def generatemodel():
         input=inputVal,
         output=pattern[0].tolist()
     )
+
+@app.route('/backtest/run', methods=['GET'])
+def generatebacktest():
+    return 'Generating Backtest'
+
+# @app.route('/backtest', methods=['PUT', 'POST'])
+# def create():
+#     try:
+#         name = request.json['name']
+#         stock_ref.document(name).set(request.json)
+    
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
+
+@app.route('/backtest/<name>', methods=['GET'])
+def read(name):
+    try:
+        stock_name = name
+        if stock_name:
+            stock = stock_ref.document(stock_name).get()
+            return jsonify(stock.to_dict()), 200
+        else:
+            stock = [doc.to_dict() for doc in stock_ref.stream()]
+            return jsonify(stock), 200
+        
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 if __name__ == "__main__":
