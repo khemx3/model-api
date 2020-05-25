6a5e70a1ad50e4c20db7cede1988b565eb5a000b
Phakapol Chuliang <phakapol.chuliang@gmail.com>
2020-05-25 15:06:44 +0700

    update env key

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from firebase_admin import credentials, firestore, initialize_app
 from model import model
+from dotenv import load_dotenv
 import os
+
 
 
 ml_model = model(20, 12)
@@ -9,16 +11,32 @@ ml_model.restore_model("models/model_10_weights")
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 8000))
- 
 
-cred = credentials.Certificate(Buffer.from(process.env.FIREBASE_CONFIG_BASE64, 'base64').toString('ascii')))
+
+load_dotenv()
+
+ENV_KEYS = {
+    "type": "service_account",
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+     "auth_uri": os.environ.get("FIREBASE_AUTH_URI")
+}
+
+
+cred = credentials.Certificate(ENV_KEYS)
 default_app = initialize_app(cred)
 db = firestore.client()
 stock_ref = db.collection('stock')
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized and deployed to Heroku'
+    return "hello world"
 
 @app.route("/model", methods=['POST'])
 def generatemodel():
